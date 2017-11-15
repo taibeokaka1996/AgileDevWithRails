616d67acbd5e149e260363b9d1a5c27375d2c86c
Tai Nguyen-Trung <taint@pc-317.local.elarion.com>
2017-11-15 17:16:53 +0700

    Complete Test Fixtures page 118

--- DepotApp/app/models/product.rb
+++ DepotApp/app/models/product.rb
@@ -1,2 +1,21 @@
 class Product < ApplicationRecord
+    ## presence: kiểm tra xem thông tin có nào bỏ trống không?
+    validates :title, :description, :image_url, presence:{
+        message: 'Title, Description, and image URL fields arent empty'
+    }
+
+
+    validates :price, numericality: {
+        greater_than_or_equal_to: 0.01,
+        message: 'The price is a valid number not less than $0.01.'
+    }
+
+    ## uniqueness : kiểm tra xem title có trùng nhau không
+    validates :title, uniqueness: {
+        message: 'The title is unique among all products'    
+    }
+    validates :image_url, allow_blank: true, format:{
+        with: %r{\.(gif|jpg|png)\Z}i,
+        message: 'must be a URL for GIF, JPG or PNG image'
+    }
 end
--- DepotApp/test/controllers/products_controller_test.rb
+++ DepotApp/test/controllers/products_controller_test.rb
@@ -3,7 +3,14 @@ require 'test_helper'
 class ProductsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @product = products(:one)
+    @update = {
+      title: 'Lorem Ipsum'
+      description: 'Wibbles are fun!',
+      image_url: 'lorem.jpg',
+      price: 19.95
+    }
   end
+
 
   test "should get index" do
     get products_url
@@ -17,32 +24,35 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
   test "should create product" do
     assert_difference('Product.count') do
-      post products_url, params: { product: {  title: @product. title, description: @product.description, image_url: @product.image_url, price: @product.price } }
+      post products_url, params: { product: @update }
     end
 
     assert_redirected_to product_url(Product.last)
   end
 
-  test "should show product" do
-    get product_url(@product)
-    assert_response :success
-  end
+  
+  #.  . .
+  #test "should show product" do
+  #  get product_url(@product)
+  #  assert_response :success
+  #end
 
-  test "should get edit" do
-    get edit_product_url(@product)
-    assert_response :success
-  end
-
+  #test "should get edit" do
+  #  get edit_product_url(@product)
+  #  assert_response :success
+  #end
+  #.  . .
   test "should update product" do
-    patch product_url(@product), params: { product: {  title: @product. title, description: @product.description, image_url: @product.image_url, price: @product.price } }
+    patch product_url(@product), params: { product: @update }
     assert_redirected_to product_url(@product)
   end
 
-  test "should destroy product" do
-    assert_difference('Product.count', -1) do
-      delete product_url(@product)
-    end
+  #  
+  #test "should destroy product" do
+  #  assert_difference('Product.count', -1) do
+  #    delete product_url(@product)
+  #  end
 
-    assert_redirected_to products_url
-  end
+   # assert_redirected_to products_url
+  #end
 end
