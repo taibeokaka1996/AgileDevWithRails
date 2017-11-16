40db7a361e5ca54d0f565180d6a39467fe6d51cf
Tai Nguyen-Trung <taint@pc-317.local.elarion.com>
2017-11-16 11:33:09 +0700

    Continue with Task D: CartCreation Chapter9 Page 140

--- DepotApp/config/routes.rb
+++ DepotApp/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'products/index'
+  resources :line_items
+  resources :carts
+  root 'store#index', as: 'store_index'
   resources :products
-  root 'products#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
