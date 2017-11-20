d8d060bb185fd8f585c367f92c006dbd6568d625
Tai Nguyen-Trung <taint@pc-317.local.elarion.com>
2017-11-20 11:49:19 +0700

    Continuing Task H page 214

---
+++ DepotApp/config/environments/environments.rb
@@ -0,0 +1,15 @@
+config.action_mailer.delivery_method = :smtp
+
+DepotApp::Application.configure do
+    config.action_mailer.delivery_method = :test 
+
+    config.action_mailer.smtp_settings = {
+        address: "smtp.gmail.com",
+        port: 587,
+        domain: "domain.of.sender.net",
+        authentication: "plain",
+        user_name: "taibeoka",
+        password: "secret",
+        enable_starttls_auto: true 
+    }
+end
